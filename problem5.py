26af6e4b1563c97412712fab6d4e19de33d46444
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-03-27 23:39:31 +0530

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,9 @@
+#weather activity suggestion :
+
+weather = "sunny"
+
+if weather == "sunny":
+    activity ="Go for a walk"
+elif weather == "rainy":
+    activity = "eat pakodi"
+print("so u can :",activity)
---
+++ problem6.py
@@ -0,0 +1,23 @@
+distance =int(input("enter the number : "))
+if distance == 3:
+    print("Go for awalk")
+elif distance == 5:
+    print("use a bike for going somewhere")
+elif distance == 7:
+    print("use a bike for going somewhere")
+elif distance == 10:
+    print("use a bike for going somewhere")
+elif distance == 12:
+    print("use a bike for going somewhere")
+elif distance <= 15:
+    print("use a bike for going somewhere")
+elif distance > 15:
+    print("use a car for going somewhere")
+
+
+
+
+
+
+
+
