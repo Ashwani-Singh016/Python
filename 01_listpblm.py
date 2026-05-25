a581fcde7081183971ab82b17cf812a6a780e993
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-05-25 14:31:57 +0530

    Add files via upload

---
+++ 01_listpblm.py
@@ -0,0 +1,19 @@
+# LIST PROBLEMS
+
+a = [1, 2, 3, 4, 5] # List of integers
+b = ['apple', 'banana', 'cherry'] # List of strings
+c = [1, 'hello', 3.14, True] # Mixed data types
+
+# print(a)
+# print(b)
+# print(c)
+
+# +++++++++++++++++++  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+# CREATING LIST WITH REPEATED ELEMENTS
+
+a = [2] * 5
+b = [0] * 7
+
+print(a)
+print(b)
