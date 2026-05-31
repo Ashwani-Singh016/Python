f87a4a541cecbad688ec5fdeb432083e3eb112b6
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-05-31 23:44:27 +0530

    Add files via upload

---
+++ 02_accesslist.py
@@ -0,0 +1,6 @@
+# List in the python is used as a arrays becoz python does not contain any arrays..
+
+a = [10, 20, 30, 40, 50]
+print(a[0])    
+print(a[-1])
+print(a[1:4])   # elements from index 1 to 3
